368f54090e3d3d2486be6b72c8cb9ff71cac9c6e
손동민 <sondmmm0308@gmail.com>
2025-11-16 17:08:56 +0900

    [DOcs] 7주차 발표자료 업로드 - #28

--- contents/dongmin/src/main/java/server/dongmin/domain/basket/dto/res/BasketItemResponse.java
+++ contents/dongmin/src/main/java/server/dongmin/domain/basket/dto/res/BasketItemResponse.java
@@ -1,14 +1,25 @@
 package server.dongmin.domain.basket.dto.res;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import server.dongmin.domain.basket.entity.BasketItem;
 
 import java.math.BigDecimal;
 
+@Schema(description = "장바구니 품목 정보 응답 DTO")
 public record BasketItemResponse(
+        @Schema(description = "장바구니 ID", example = "1")
         Long basketItemId,
+
+        @Schema(description = "장바구니 품목 ID", example = "1")
         Long menuId,
+
+        @Schema(description = "장바구니 품목 수량", example = "1")
         int quantity,
+
+        @Schema(description = "장바구니 품목 단일 가격", example = "1000")
         BigDecimal price, // 단일 가격
+
+        @Schema(description = "수량 X 단일 가격", example = "1000")
         BigDecimal totalPrice // 수량 * 가격
 ) {
     public static BasketItemResponse from(BasketItem basketItem) {
---
+++ contents/dongmin/src/main/java/server/dongmin/domain/user/controller/UserControllerSpecification.java
@@ -0,0 +1,7 @@
+package server.dongmin.domain.user.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "UserController", description = "유저 컨트롤러")
+public interface UserControllerSpecification {
+}
